f388a2106c5fe2d6453f205d38a051c46a521398
Ross Young <rosswillyoung@gmail.com>
2019-11-05 10:56:27 -0500

    Added Json to read the client id, secret, user agent

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-import praw
 import pprint
-
+import json
+import praw
              
-for submission in reddit.subreddit('reddevils').hot(limit=1):
-    print(submission.title)
-    pprint.pprint(vars(submission))
+with open('keys.json') as f:
+    data = json.load(f)
+    client_id = data["data"]["client_id"]
+    client_secret = data["data"]["client_secret"]
+    user_agent = data["data"]["user_agent"]
+
+print(user_agent)
+
+# for submission in reddit.subreddit('reddevils').hot(limit=1):
+#     print(submission.title)
+#     pprint.pprint(vars(submission))
