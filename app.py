8434d933ee84e2b55d3cb0144e3ba2bfb26970f3
Ross Young <rosswillyoung@gmail.com>
2019-11-06 15:34:50 -0500

    Added some logic to get comments/second level comments and a way to quit

--- app.py
+++ app.py
@@ -2,24 +2,44 @@ import pprint
 import json
 import praw
              
+# Get PRAW client information from keys.json file
 with open('keys.json') as f:
     data = json.load(f)
     client_id = data["data"]["client_id"]
     client_secret = data["data"]["client_secret"]
     user_agent = data["data"]["user_agent"]
 
+# Connect to PRAW with client information
 reddit = praw.Reddit(client_id=client_id,
                     client_secret=client_secret,
                     user_agent=user_agent)
 
-for submission in reddit.subreddit('reddevils').hot(limit=10):
+# Gets comments from a submission
+def get_comments(submission):
+    for top_level_comment in submission.comments:
+        print(top_level_comment.body)
+        second_level = input("Get second level comments? ")
+        if (second_level == "Y" or second_level == "y"):
+            for second_level_comment in top_level_comment.replies:
+                print(second_level_comment.body)
+        elif (second_level == "quit" or second_level == "Quit"):
+            break
+        else:
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            next
+
+sub = input("Which subreddit would you like to view? ")
+
+for submission in reddit.subreddit(sub).hot(limit=10):
     print(submission.title)
     print(submission.selftext)
-    if (input('Load Comments?') == "y"):
-        for top_level_comment in submission.comments:
-            print(top_level_comment.body)
-            print('###########################')
+    user_wants_comments = input("Load Comments? (y/Y for yes) ")
+    if (user_wants_comments == "y" or user_wants_comments == "Y"):
+        get_comments(submission)
+    elif(user_wants_comments == "quit" or user_wants_comments == "Quit"):
+        break
     else:
         next
     print("Loading next title...")
     # pprint.pprint(vars(submission))
+
